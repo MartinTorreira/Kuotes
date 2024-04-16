3e4448b1f18fc2c557cb6318f6fce444f40c295e
Martin Torreira Portos <Martin Torreira Portos>
2024-04-16 14:51:15 +0200

    Styles to quote list

--- frontend/src/components/quote/ShowQuotes.js
+++ frontend/src/components/quote/ShowQuotes.js
@@ -43,22 +43,22 @@ const ShowQuotes = () => {
     };
 
     return (
-        <div className="bg-white shadow mt-20 md:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto max-h-[400px] overflow-y-auto ">
-            <h2 className="text-xl font-semibold text-gray-900 px-4 py-2 border-b border-gray-200 text-center bg-gray-100 border ">Active quotes</h2>
+        <div className="bg-white dark:bg-[#28242c] shadow mt-20 md:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto max-h-[400px] overflow-y-auto ">
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-gray-100 px-4 py-2 border-b border-gray-200 text-center bg-gray-100 dark:bg-[#332e38] border dark:border-gray-700 ">Active quotes</h2>
             <ul>
                 {quotes.map((quote, index) => (
-                    <li key={index} className={index > 0 ? "cursor-pointer hover:bg-gray-50 border-t border-gray-200" : "cursor-pointer hover:bg-gray-50"}>
+                    <li key={index} className={index > 0 ? "cursor-pointer hover:bg-gray-50 dark:hover:bg-[#332e38] border-t border-gray-200 dark: border-gray-700 transition duration-300 ease-in-out" : "dark:hover:bg-[#332e38] cursor-pointer hover:bg-gray-50"}>
                         <div className="px-2 py-3 sm:px-6">
                             <div className="flex items-center justify-between">
-                                <h3 className="sm:text-lg leading-6 font-medium text-gray-900 hover:underline">{quote.title}</h3>
-                                <p className="mt-1 max-w-xs sm:max-w-2xl text-xs sm:text-xxs text-gray-500 ">{dateConverter(quote.date)}</p>
+                                <h3 className="sm:text-lg leading-6 text-gray-900 dark:text-gray-200 ">{quote.title}</h3>
+                                <p className="mt-1 max-w-xs sm:max-w-2xl text-xs sm:text-xxs text-gray-400 dark:text-gray-00 ">{dateConverter(quote.date)}</p>
                             </div>
                             <div className="mt-2 flex items-center justify-between">
                                 <p className={`bg-gray-200 rounded-full px-2 py-1 mt-1 max-w-xs sm:max-w-2xl text-xs sm:text-xxs text-gray-500 ${getImportanceColor(quote.importance)}`}>
                                     <span>{quote.importance}</span>
                                 </p>
                                 <button onClick={() => removeQuote(quote)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24" className='hover:fill-red-500'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24" className='hover:fill-red-600 dark:hover:fill-red-600 dark:fill-gray-200'>
                                         <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.1 5.9 22 7 22 L 17 22 C 18.1 22 19 21.1 19 20 L 19 7 L 5 7 z M 8 9 L 10 9 L 10 20 L 8 20 L 8 9 z M 14 9 L 16 9 L 16 20 L 14 20 L 14 9 z"></path>
                                     </svg>
                                 </button>
